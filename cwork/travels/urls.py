61d3c70a94c115a49ead60ecfe6b65f67066025f
Egor Kroshkin <kroshkin.egor@yandex.ru>
2022-07-10 18:38:44 +0300

    CRUD

--- cwork/travels/urls.py
+++ cwork/travels/urls.py
@@ -4,10 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.login),
-    path('login/', views.login,name='login'),
-    path('register/', views.login, name='register'),
     path('main/', views.main),
     path('offers/<slug:slug_city_offers>', views.city_offers, name='city_offers-detail'),
     path('offer/<slug:slug_tour>', views.tour, name='tour-detail'),
-
+    path('list_order/', views.list_order),
 ]
--- cwork/travels/views.py
+++ cwork/travels/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import City, Offer, Service, Order
 from django.http import HttpResponseRedirect
 from .forms import OrderForm
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
 
 def main(request):
 	cities = City.objects.all()
@@ -54,8 +49,6 @@ def login(request):
 	# return render(request, 'travels/login.html')	
 
 
-
-class RegisterUser(CreateView):
-	form_class = UserCreationForm
-	template_name = 'travels/register.html'
-	success_url = reverse_lazy('login')
+def list_order(request):
+	orders = Order.objects.all()
+	return render(request, 'travels/list_order.html' , {'orders': orders})
